fd2ee280379e3cf16c0f498027cba21610ea0d51
prgao <peiran_gao@me.com>
2013-07-12 15:18:39 -0700

    updated with more useful modules for vim

--- setup.sh
+++ setup.sh
@@ -1,4 +1,22 @@
 cd vim/bundle
+#vim-slime
 git clone git://github.com/jpalardy/vim-slime.git
+
+#vim-sensible
 git clone git://github.com/tpope/vim-sensible.git
+
+#multiple-cursor
 git clone git://github.com/terryma/vim-multiple-cursors.git
+
+#NERDTree
+git clone git://github.com/scrooloose/nerdtree.git
+
+#scala highlighting
+git clone git://github.com/derekwyatt/vim-scala.git
+
+#command-T fuzzy search
+git clone git://git.wincent.com/command-t.git
+cd command-t/ruby/command-t
+ruby extconf.rb
+make
+cd ../../../
